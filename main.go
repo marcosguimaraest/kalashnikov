7fae5a24f3968b9cc9c58be22c21d56377d99d23
Marcos Trindade <marcosguimaraest04@gmail.com>
2024-11-05 23:56:00 -0300

    remove comments

--- main.go
+++ main.go
@@ -23,16 +23,11 @@ func eventHandler(evt interface{}) {
 }
 
 func main() {
-	// |------------------------------------------------------------------------------------------------------|
-	// | NOTE: You must also import the appropriate DB connector, e.g. github.com/mattn/go-sqlite3 for SQLite |
-	// |------------------------------------------------------------------------------------------------------|
-
 	dbLog := waLog.Stdout("Database", "DEBUG", true)
 	container, err := sqlstore.New("sqlite3", "file:examplestore.db?_foreign_keys=on", dbLog)
 	if err != nil {
 		panic(err)
 	}
-	// If you want multiple sessions, remember their JIDs and use .GetDevice(jid) or .GetAllDevices() instead.
 	deviceStore, err := container.GetFirstDevice()
 	if err != nil {
 		panic(err)
